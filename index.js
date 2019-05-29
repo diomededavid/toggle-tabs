89601686bce4e31ca2609619f06b3522d7126f12
diomededavid <david@d2creativedesign.com>
2019-05-28 23:36:40 -0700

    Add tabs script

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 // Import stylesheets
 import './style.css';
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+// // Write Javascript code!
+// const appDiv = document.getElementById('app');
+// appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
+
+const element = document.querySelector('#tabs');
+
+
+element.addEventListener('click', function (event) {
+  let activeTabs = document.querySelectorAll('.active');
+
+  activeTabs.forEach(function (tab) {
+    tab.className = tab.className.replace('active', '');
+  });
+  event.target.parentElement.className == 'active';
+  document.getElementById(event.target.href.split('#')[1]).className += ' active';
+
+}, false);
